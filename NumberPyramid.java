562052f00afc1e8042f9ba2f2594784681272f0d
T LAKSHMI VYSHNAVI <24wh1a05aa@bvrithyderabad.edu.in>
2025-08-14 13:00:30 +0530

    Add files via upload

---
+++ NumberPyramid.java
@@ -0,0 +1,24 @@
+package intro;
+
+import java.util.Scanner;
+
+public class NumberPyramid {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int rows=sc.nextInt();
+        sc.close();
+
+        for (int i = 1; i <= rows; i++) {
+            
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
